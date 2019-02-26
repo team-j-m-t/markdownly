423f65cce02b3de33aaeca8866e4ff8136256465
Marcy Silverman <marcysilve225@gmail.com>
2019-02-26 15:39:25 -0800

    added the add document and delete button

--- src/components/markdown/Document.js
+++ src/components/markdown/Document.js
@@ -23,6 +23,7 @@ export default class Document extends PureComponent {
           <Editor markdown={markdown} updateMarkdown={this.updateMarkdown} />
           <Preview markdown={markdown} />
         </div>
+          <button>Delete Document</button> 
       </>
     );
   }
