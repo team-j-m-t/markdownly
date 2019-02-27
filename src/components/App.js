d3e16f13194879cad2162cb15737c560fee8e058
Teonna Zaragoza <teonna.zaragoza@gmail.com>
2019-02-27 14:35:58 -0800

    took input

--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,6 @@ import EditDoc from './containers/EditDoc';
 export default function App() {
   return (
     <>
-      <div>
-        <label> Title of Document</label>
-        <input type="text"></input>
-      </div>
       <h1>Markdown Editor</h1>
       <EditDoc />
     </>
